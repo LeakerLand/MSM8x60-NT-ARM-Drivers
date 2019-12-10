3f42d0d5b03db47130d28279679919e83bc9cda1
Gustave Monce <gustave.monce@outlook.com>
2019-12-10 23:16:39 +0100

    More drivers

--- support-desktop/Setup/Oobe.cmd
+++ support-desktop/Setup/Oobe.cmd
@@ -11,23 +11,23 @@ set /p osbuild=<psout.txt
 del psout.txt
 
 if %osbuild% gtr 18908 (
-    \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\WA\qcmbb.wp8974.inf QCMS\QCOM0EA0
+    \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\WA\qcmbb.wa8960.inf ACPI\QCOM2072
 ) else (
-    \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\WP\qcmbb.wp8974.inf QCMS\QCOM0EA0
+    \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\WP\qcmbb.wp8960.inf ACPI\QCOM2072
 )
 
 cmd.exe /c \Windows\OEM\IHVDriversOobe.cmd
 
-\Windows\OEM\devcon.exe status @QCMS\QCOM0EA0\*1 | findstr "43" > nul
+\Windows\OEM\devcon.exe status @ACPI\QCOM2072\*1 | findstr "43" > nul
 if %errorlevel%==0 (
-    \Windows\OEM\devcon.exe remove @QCMS\QCOM0EA0\*1
+    \Windows\OEM\devcon.exe remove @ACPI\QCOM2072\*1
     \Windows\OEM\devcon.exe dp_add \Windows\OEM\Drivers\qcmbbnull.inf
     \Windows\OEM\devcon.exe rescan
 )
 
-\Windows\OEM\devcon.exe status @QCMS\QCOM0EA0\*0 | findstr "43" > nul
+\Windows\OEM\devcon.exe status @ACPI\QCOM2072\*0 | findstr "43" > nul
 if %errorlevel%==0 (
-    \Windows\OEM\devcon.exe remove @QCMS\QCOM0EA0\*0
+    \Windows\OEM\devcon.exe remove @ACPI\QCOM2072\*0
     \Windows\OEM\devcon.exe dp_add \Windows\OEM\Drivers\qcmbbnull.inf
     \Windows\OEM\devcon.exe rescan
 )
